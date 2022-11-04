3142c1720ffdc6cd3a9b367e462955324ea47cf0
salvik21 <86931898+salvik21@users.noreply.github.com>
2022-11-04 22:40:07 +0200

    fixed using your commits 3

--- src/componets/Molecules/NavPanel/NavPanel.jsx
+++ src/componets/Molecules/NavPanel/NavPanel.jsx
@@ -41,7 +41,7 @@ function NavPanel() {
                 <Divider />
                 <Hamburger />
             </div>
-            <CartMenu refCartMenu={menuRef} cartMenuClass={showCartMenu ? "" : "hide"} />
+            <CartMenu refCartMenu={menuRef} cartMenuClass={showCartMenu ? "" : "-hide"} />
         </div>
     );
 }
